b56d283203a81b2bb74b237f87960b0c43b9e269
Sae-byeol <toquf0424@naver.com>
2022-03-05 17:40:47 +0900

    문답페이지 ui

--- src/pages/WriteIndex.js
+++ src/pages/WriteIndex.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import '../WriteIndex.css';
-
+import '../Login.css';
 const WriteIndex = () => {
-  return (
-    <div className="App">
-    <div className='container'>
-        <div className='innerContainer'>
-            <div className="writeIndexBox">
-                <div className='writeIndexTextBox'>
-                <h2>지금 가장 먹고싶은 음식은?</h2>
-                <input type="text" name="text" className='qnaInput'></input>
-                </div>
-            </div>
+  
+    return (
+      <div className="write container white-border-container">
+       
+        <div className="writeLine"> 
+              <div className='writeIndexTextBox'>
+                <div className='question'>Q1.당신의 이름은?</div>
+                <div className='question inputMark'> &gt;&gt;</div>
+                <textarea type="text" name="text" className='qnaInput'></textarea>
+                <div className='nextBtn'>다음</div>
+              </div>
         </div>
-    </div>
-    </div>
-  )
+       
+      </div>
+    );
+  
 }
 
 export default WriteIndex
