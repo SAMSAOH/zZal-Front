f768f6d5a76e7882e0bcf9d8594995b83e4fda7c
Sae-byeol <toquf0424@naver.com>
2022-03-05 20:48:39 +0900

    마이페이지 ui

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "../Mypage.css";
 
 const MyPage = () => {
+    //임시 데이터
     const [wills, setWills]=useState([
         {date:"2022-03-05"},
         {date:"2023-03-05"},
@@ -10,17 +11,21 @@ const MyPage = () => {
     const renderWills=wills.map((will)=>{
         return(
             <div className="renderWills">
-                <div className='willsForm'>
-                    <div className='willsFormTitle'>{will.date}의 유서</div>
-                    <button className='willsFormBtn'></button>
-                </div>
+                    <div className='willsFormTitle'>
+                        {will.date}의 유서
+                    </div>
+                    <button className='willsFormBtn'> &gt;&gt;</button>
             </div>
         )
     });
   return (
-    <div className='container mypage-container mypage'>
-        <div className='myPageTitle'>유서함</div>
+    <div className='container mypage'>
+       <div className="myPageTitle">
+			<div>유서함</div>
+		</div>
+        
         <div className="myPageMap">{renderWills}</div>
+        <button className="blue-btn myPageBlueBtn">작성 완료</button>
     </div>
   )
 }
