41534902709502ff6e01445ad1b44b2fefc6b2ff
runru1030 <runru1030@gmail.com>
2022-03-05 16:32:41 +0900

    refact : use KaKaoBtn component

--- src/components/Result/KakaoModal.jsx
+++ src/components/Result/KakaoModal.jsx
@@ -1,6 +1,6 @@
+import KakaoBtn from "../commons/KakaoBtn";
 import Modal from "../commons/Modal";
 const KakaoModal = ({ setIsOpen }) => {
-	const kauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_KEY}&redirect_uri=http://localhost:3000/&response_type=code`;
 	const handleDelClick = () => {
 		setIsOpen(false);
 	};
@@ -11,9 +11,7 @@ const KakaoModal = ({ setIsOpen }) => {
 					x
 				</button>
 				<span>배달하기 기능은 로그인 이후 가능합니다.</span>
-				<a href={kauthUrl} className="border-btn" id="kakao-btn">
-					카카오 로그인
-				</a>
+				<KakaoBtn />
 			</div>
 		</Modal>
 	);
