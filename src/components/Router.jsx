4196a677d95eddc65c72df70412f3237e2a40c61
runru1030 <runru1030@gmail.com>
2022-03-05 16:02:26 +0900

    chore : add router's element

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import Result from "../pages/Result";
 const AppRouter = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<></>} />
+				<Route path="/" element={<Main />} />
 				<Route path="/write" element={<></>}>
 					<Route path=":idx" element={<></>} />
 					<Route path="will" element={<></>} />
 				</Route>
-				<Route path="/result" element={<></>} />
+				<Route path="/result" element={<Result />} />
 				<Route path="/delivery" element={<></>} />
-				<Route path="/login" element={<></>} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/myPage" element={<></>} />
 			</Routes>
 		</Router>
