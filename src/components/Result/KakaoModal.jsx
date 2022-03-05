b7c87191ab585bbe2df12f91f509e139bef2e092
runru1030 <runru1030@gmail.com>
2022-03-05 22:44:26 +0900

    feat: 카카오 이메일 요청 안내 문구 추가

--- src/components/Result/KakaoModal.jsx
+++ src/components/Result/KakaoModal.jsx
@@ -11,6 +11,10 @@ const KakaoModal = ({ setIsOpen }) => {
 					x
 				</button>
 				<span>배달하기 기능은 로그인 이후 가능합니다.</span>
+				<small className="yellow-text">
+					**로그인 시, email 제공 동의를 해주셔야 메일을 통해 배달하기가
+					가능합니다.**
+				</small>
 				<KakaoBtn redirectUri={"/delivery"} />
 			</div>
 		</Modal>
