7d14479bc48e8a084df1401132699f480b28921b
runru1030 <runru1030@gmail.com>
2022-03-05 20:16:04 +0900

    feat: kakao Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 import KakaoBtn from "../components/commons/KakaoBtn";
 import LogoWrapper from "../components/Login/LogoWrapper";
 import "../Login.css";
+import { kakaoLoginHandler } from "../utils/KakaoLogin";
 const Login = () => {
+	const location = useLocation();
+	const params = new URLSearchParams(location.search);
+	const code = params.get("code");
+
+	useEffect(() => {
+		if (code) {
+			kakaoLoginHandler(code).then((res) => {
+				loginInfoHandler(res.userId);
+			});
+		}
+	}, []);
+
+	const navigate = useNavigate();
+	const loginInfoHandler = (userId) => {
+		window.localStorage.setItem(
+			"userId",
+			JSON.stringify({
+				userId: userId,
+			})
+		);
+		navigate("/myPage");
+	};
+
 	return (
 		<div className="login white-border-container">
 			<LogoWrapper />
---
+++ src/utils/KakaoLogin.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export const kakaoLoginHandler = async (code) => {
+	const data = {
+		grant_type: "authorization_code",
+		client_id: process.env.REACT_APP_KAKAO_REST_KEY,
+		redirect_uri: "http://localhost:3000/login",
+		code: code,
+	};
+	const queryString = Object.keys(data)
+		.map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(data[k]))
+		.join("&");
+	return new Promise(async (resolve, rejects) => {
+		const { data } = await axios.post(
+			"https://kauth.kakao.com/oauth/token",
+			queryString,
+			{
+				headers: {
+					"Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+				},
+			}
+		);
+		/* 서버 요청
+		await axios
+			.post("/auth/kakao", {
+				kakaoAccessToken: data.access_token,
+			})
+			.then((res) => {
+				resolve({ userId: res.data.userId });
+			}); */
+		/* dummy data */
+		resolve({ userId: 1 });
+	});
+};
