737935db944c369c16c781a09b3e7dee29cf7759
Sae-byeol <toquf0424@naver.com>
2022-05-03 02:21:34 +0900

    chore: apply styled-component of main

--- src/components/Main/MentContainer.jsx
+++ src/components/Main/MentContainer.jsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
+import { ColContainer } from "../commons/Container";
 
 const MentContainer = () => {
 	return (
-		<Section className="text-wrapper">
+		<Section>
+			<TextWrapper>
 			<li>◆◇ 나만의 유서 남기기</li>
 			<li>◆◇ z세대는 유서를 공유한다</li>
 			<li>◆◇ z세대는 디지털로 유서쓴다</li>
 			<li>◆◇ z세대는 디지털로 유서쓴다</li>
 			<li>◆◇ z세대는 유서를 배달한다</li>
+			</TextWrapper>
 		</Section>
 	);
 };
@@ -18,3 +21,9 @@ const Section = styled.section`
 	display: flex;
 	flex-direction: column;
 `;
+const TextWrapper=styled(ColContainer)`
+	color: #F5DE0E;
+	justify-content: center;
+	line-height: 1.5;
+	padding: 30vh 0;
+`
