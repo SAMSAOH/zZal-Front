a482c3d8947918546c6233613bf0d31d5774f366
runru1030 <runru1030@gmail.com>
2022-05-05 17:36:53 +0900

    design : delivery page

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -11,9 +11,7 @@ import {
 	Container,
 	RowContainer,
 } from "../components/commons/Container";
-import Flex from "../components/commons/Flex";
 import DeliveryModal from "../components/Result/DeliveryModal";
-import "../Delivery.css";
 import { login } from "../modules/userSlice";
 const Delivery = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -43,21 +41,19 @@ const Delivery = () => {
 		<>
 			<BGContainer dir="column" alignCenter width="100%">
 				<Head>배달하기</Head>
-				<TextWrapper width="80%">
-					<h3>{">>"}배달하기란?</h3>
+				<TextWrapper width="90%">
+					<h3>{">> 배달하기란?"}</h3>
 					<ContentWrapper>
 						1년 마다, 나의 메일로 <br /> 유서를 배달해줍니다.
 						<br /> <br /> 험난한 지구에서 살아남은 <br /> 당신에게 보내는 유서
 						<br /> <br /> 1년전, 나를 돌아보세요.
 					</ContentWrapper>
-					<Section>
-						<YellowText>
-							**배달 메일
-							<br /> <br /> 카카오 계정 이메일로 발송됩니다.
-						</YellowText>
-					</Section>
+					<NoticeBox>
+						** 배달 메일
+						<br /> <br /> 카카오 계정 이메일로 발송됩니다.
+					</NoticeBox>
 				</TextWrapper>
-				<RowContainer className="btn-wrapper">
+				<RowContainer padding="20px 0">
 					<BorderBlueBtn onClick={handleClickDelivery}>배달 신청</BorderBlueBtn>
 				</RowContainer>
 			</BGContainer>
@@ -68,28 +64,33 @@ const Delivery = () => {
 
 export default Delivery;
 const BGContainer = styled(Container)`
-	background-image: url("/public/img/mail.png");
+	background-image: url("/img/mail.png");
 	background-repeat: no-repeat;
 	background-position: center;
 `;
 const TextWrapper = styled(ColContainer)`
 	flex: 0.9;
+	padding: 20px;
 	justify-content: flex-start;
+	background-color: #00000040;
 `;
-const Section = styled.section`
-	width: 90%;
+const NoticeBox = styled.p`
+	width: 100%;
+	border: solid 3px #f5de0e;
+	padding: 20px;
+	color: #f5de0e;
+	background-color: #ffffff20;
+	font-size: small;
 `;
 const ContentWrapper = styled.p`
 	color: white;
 	line-height: 1.5;
 	margin: 20px 0;
+	flex: 1;
 `;
 const Head = styled.h2`
 	border-bottom: solid 3px white;
 	width: 90%;
 	text-align: center;
 	padding-bottom: 20px;
-`;
-const YellowText = styled.p`
-	color: #f5de0e;
 `;
